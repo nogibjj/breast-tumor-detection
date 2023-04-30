603518b4a3cb844d94210cef639af1a93cef8e9b
qu-genesis <60377132+qu-genesis@users.noreply.github.com>
2023-04-30 17:40:49 -0400

    Add files via upload

--- Model/FasterRCNN/src/model_softmax.py
+++ Model/FasterRCNN/src/model_softmax.py
@@ -70,7 +70,9 @@ class MotionBlur(torch.nn.Module):
         self.logits = torch.nn.Parameter(
             torch.ones(8, device=DEVICE), requires_grad=True
         )
-        self.probs = F.softmax(self.logits, dtype=torch.float).to(DEVICE)
+        self.probs = torch.nn.Parameter(F.softmax(self.logits, dtype=torch.float).to(DEVICE), 
+        requires_grad=True
+        )
 
         # Initialize motion convlutions and final image
         self.final_image = torch.nn.Parameter(torch.zeros(1, 512, 512)).to(DEVICE)
