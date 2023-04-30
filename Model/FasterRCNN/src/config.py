26136ec077b35ea9d6a935bd3f2df84e4b950036
qu-genesis <60377132+qu-genesis@users.noreply.github.com>
2023-04-29 21:32:40 -0400

    Change config for colab

--- Model/FasterRCNN/src/config.py
+++ Model/FasterRCNN/src/config.py
@@ -1,15 +1,15 @@
 import torch
 
-BATCH_SIZE = 4  # increase / decrease according to GPU memeory
+BATCH_SIZE = 16  # increase / decrease according to GPU memeory
 RESIZE_TO = 512  # resize the image for training and transforms
 NUM_EPOCHS = 100  # number of epochs to train for
 
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 # training images and XML files directory
-TRAIN_DIR = "/workspaces/breast-tumor-detection/Model/FasterRCNN/data/train"
+TRAIN_DIR = "/content/breast-tumor-detection/Model/FasterRCNN/data/train"
 # validation images and XML files directory
-VALID_DIR = "/workspaces/breast-tumor-detection/Model/FasterRCNN/data/val"
+VALID_DIR = "/content/breast-tumor-detection/Model/FasterRCNN/data/val"
 # classes: 0 index is reserved for background
 CLASSES = ["background", "tumor"]
 NUM_CLASSES = 2
@@ -17,7 +17,7 @@ NUM_CLASSES = 2
 VISUALIZE_TRANSFORMED_IMAGES = False
 
 # location to save model and plots
-OUT_DIR = "/workspaces/breast-tumor-detection/Model/FasterRCNN/outputs"
+OUT_DIR = "/content/breast-tumor-detection/Model/FasterRCNN/outputs"
 
 SAVE_PLOTS_EPOCH = 100  # save loss plots after these many epochs
 SAVE_MODEL_EPOCH = 100  # save model after these many epochs
